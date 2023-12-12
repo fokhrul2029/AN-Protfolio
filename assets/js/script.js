1be540dacf926163633baf8aebfe322f5688278e
fokhrul2029 <ifokhrul.2029@gmail.com>
2023-12-12 15:04:23 +0600

    update navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+// Navigation Js-----------
+window.onscroll = () => {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.querySelector("#navbar").style.boxShadow = " 0px 0px 20px 0px #0000005e   ";
+  } else {
+    document.querySelector("#navbar").style.boxShadow =
+      " 0 0 0  transparent ";
+  }
+};
+
+// Toast of Contact Form
 const toastTrigger = document.getElementById("liveToastBtn");
 const toastLiveExample = document.getElementById("liveToast");
 
