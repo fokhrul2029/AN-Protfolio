48e66fce0fcbdf74005b1957db312c9d75396128
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-01-11 14:47:02 +0600

    blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,4 @@
+
 // Get the button
 let mybutton = document.getElementById("myBtn");
 
@@ -17,3 +18,99 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+// <!------------- Review Of my Blog----------------------->
+
+window.addEventListener("DOMContentLoaded",() => {
+	const starRating = new StarRating("form");
+});
+
+class StarRating {
+	constructor(qs) {
+		this.ratings = [
+			{id: 1, name: "Terrible"},
+			{id: 2, name: "Bad"},
+			{id: 3, name: "OK"},
+			{id: 4, name: "Good"},
+			{id: 5, name: "Excellent"}
+		];
+		this.rating = null;
+		this.el = document.querySelector(qs);
+
+		this.init();
+	}
+	init() {
+		this.el?.addEventListener("change",this.updateRating.bind(this));
+
+		// stop Firefox from preserving form data between refreshes
+		try {
+			this.el?.reset();
+		} catch (err) {
+			console.error("Element isn’t a form.");
+		}
+	}
+	updateRating(e) {
+		// clear animation delays
+		Array.from(this.el.querySelectorAll(`[for*="rating"]`)).forEach(el => {
+			el.className = "rating__label";
+		});
+
+		const ratingObject = this.ratings.find(r => r.id === +e.target.value);
+		const prevRatingID = this.rating?.id || 0;
+
+		let delay = 0;
+		this.rating = ratingObject;
+		this.ratings.forEach(rating => {
+			const { id } = rating;
+
+			// add the delays
+			const ratingLabel = this.el.querySelector(`[for="rating-${id}"]`);
+
+			if (id > prevRatingID + 1 && id <= this.rating.id) {
+				++delay;
+				ratingLabel.classList.add(`rating__label--delay${delay}`);
+			}
+
+			// hide ratings to not read, show the one to read
+			const ratingTextEl = this.el.querySelector(`[data-rating="${id}"]`);
+
+			if (this.rating.id !== id)
+				ratingTextEl.setAttribute("hidden",true);
+			else
+				ratingTextEl.removeAttribute("hidden");
+		});
+	}
+}
+// <!------------- Review Of my Blog END----------------------->
+
+
+// Time Sectiom ---------------
+function updateTime() {
+  const dateElement = document.getElementById('date');
+  const timeElement = document.getElementById('time');
+
+  const now = new Date();
+
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = now.toLocaleDateString('en-US', options);
+
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  //For 14Hour Format--
+  const formattedTime = `${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`;
+
+  //For 12 Hour Format--
+  // const formattedTime = `${addZero(hours % 12 || 12)}:${addZero(minutes)}:${addZero(seconds)} ${hours >= 12 ? 'PM' : 'AM'}`;
+
+  dateElement.textContent = formattedDate;
+  timeElement.textContent = formattedTime;
+}
+
+function addZero(number) {
+  return number < 10 ? '0' + number : number;
+}
+
+updateTime(); // Initial call
+setInterval(updateTime, 1000); // Update every second
